4c9b05a7d405127b78a636bfe4e2397ab7957566
cluo29 <mr.cluo29@gmail.com>
2016-10-01 20:40:21 +0300

    1

---
+++ app/src/main/java/io/github/cluo29/contextdatareading/ViewReplay.java
@@ -0,0 +1,22 @@
+package io.github.cluo29.contextdatareading;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Comet on 01/10/16.
+ */
+public class ViewReplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewreplay);
+
+    }
+
+}
+
+//ml result
+//power
+//processing time
